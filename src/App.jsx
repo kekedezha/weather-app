abdb3d81c372f50c789d25b8af1223f09fe85e3f
kekedezha <dezhac6@gmail.com>
2024-07-12 14:11:26 -0400

    updated styling for app div contianer and main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,13 @@ import "./App.css";
 
 function App() {
   return (
-    <>
+    <div id="app">
       <SearchBar />
       <DateCity />
       <Description />
       <Temp />
       <ExtraInfo />
-    </>
+    </div>
   );
 }
 
